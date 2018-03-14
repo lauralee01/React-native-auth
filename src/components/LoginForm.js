2bc32b66360b27c59808dde70eadcbb15ff5ee80
lauralee01 <grace01>
2018-03-14 15:08:40 +0100

    add and edit Spinner.js file

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress() {
+		const { email, password } = this.state;
+
+		this.setState({ error: '' });
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({ error: 'Authentication Failed.'});
+						});
+					
+			
+				});
+			
+	}
 
 	render() {
 		return (
@@ -20,14 +38,18 @@ class LoginForm extends Component {
 					<Input
 						secureTextEntry
 						placeholder="password"
-						label="Password"
+				 		label="Password"
 						value={this.state.password}
 						onChangeText={password => this.setState({ password })}
 					/>
 				 </CardSection>
 
+				 <Text style={styles.errorTextStyle}>
+				 	{this.state.error}
+				 </Text>
+
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log in
 					</Button>
 				</CardSection>
@@ -36,5 +58,13 @@ class LoginForm extends Component {
 			);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
